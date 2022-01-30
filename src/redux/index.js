4c25eab9a2b6806f5a5e8feafbe20cf8c63c5ddc
Alexey <lex91179@mail.ru>
2022-01-31 00:24:32 +0300

    Updates

--- src/redux/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-
-import { composeWithDevTools } from "redux-devtools-extension";
-import {reducers} from './reducers'
-import thunk from 'redux-thunk'
-
-const composeEnhancers = composeWithDevTools(applyMiddleware(thunk));
-
-export const store = createStore(reducers, composeEnhancers);
--- src/redux/types.js
+++
@@ -1,7 +0,0 @@
-const actionTypes = {
-  SHOW_NEWS: "SHOW_NEWS",
-  CLICK_HEART: "CLICK_HEART",
-  DELET_NEWS: "DELET_NEWS",
-};
-
-export default actionTypes;
